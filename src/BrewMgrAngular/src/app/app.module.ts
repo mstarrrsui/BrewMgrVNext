e5c546bef2d86f3363daca5a3fb184a0e49d59c4
Mike Starr <mstarr@rsui.com>
2017-09-21 16:23:30 -0400

    reactive form - in progress

--- src/BrewMgrAngular/src/app/app.module.ts
+++ src/BrewMgrAngular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 
--- src/BrewMgrAngular/src/app/ingredients/hops-detail/hop-detail.component.ts
+++ src/BrewMgrAngular/src/app/ingredients/hops-detail/hop-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute, ParamMap } from '@angular/router'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { IngredientService } from '../service/ingredient.service';
 import { Hop } from '../model/hop.model';
 import { Observable } from 'rxjs/Rx';
@@ -9,11 +10,27 @@ import { Observable } from 'rxjs/Rx';
   templateUrl: './hop-detail.component.html',
   styles: [`
     .container { padding-left:20px; padding-right:20px; }
+    em {float:right; color:#E05C65; padding-left: 10px;}
+    .error input {background-color:#E3C3C5;}
+    .error ::-webkit-input-placeholder { color: #999; }
+    .error ::-moz-placeholder { color: #999; }
+    .error :-moz-placeholder { color:#999; }
+    .error :ms-input-placeholder { color: #999; }
   `]
 })
 export class HopDetailComponent implements OnInit {
 
   public hop: Hop;
+
+  public hopForm : FormGroup;
+  public name : FormControl;
+  public description : FormControl;
+  public type : FormControl;
+  public countryOfOrigin : FormControl;
+  public useIn : FormControl;
+  public betaAcidPct : FormControl;
+  public alphaAcidPct : FormControl;
+
 
   constructor(private ingredientService: IngredientService, private route: ActivatedRoute) {
 
@@ -24,8 +41,32 @@ export class HopDetailComponent implements OnInit {
     this.route.paramMap
       .switchMap((params: ParamMap) =>
         this.ingredientService.getHop(+params.get('id')))
-      .subscribe((h: Hop) => this.hop = h);
+      .subscribe((h: Hop) => this.setFormValues(h));
 
   }
 
+  private setFormValues(h: Hop) {
+
+    this.name = new FormControl(h.name,Validators.required);
+    this.description = new FormControl(h.description,Validators.required);
+    this.type = new FormControl(h.type,Validators.required);
+    this.countryOfOrigin = new FormControl(h.countryOfOrigin,Validators.required);
+    this.useIn = new FormControl(h.useIn,Validators.required);
+    this.betaAcidPct = new FormControl(h.betaAcid,Validators.required);
+    this.alphaAcidPct = new FormControl(h.alphaAcid,Validators.required);
+
+    this.hopForm = new FormGroup({
+      name: this.name,
+      description: this.description,
+      type: this.type,
+      countryOfOrigin: this.countryOfOrigin,
+      useIn: this.useIn,
+      betaAcidPct: this.betaAcidPct,
+      alphaAcidPct: this.alphaAcidPct
+    });
+  }
+
+  public onFormSubmit(formValues) {
+    console.log(formValues);
+  }
 }
